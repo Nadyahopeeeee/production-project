3b883842276013dbe91a0e5e315d9122163d3ae1
Nadezhda Semenenko <semenenko.n@routeamgroup.com>
2023-10-13 00:41:43 +0300

    add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+import webpackDevServer from 'webpack-dev-server'
+
+import path from 'path';
+import { BuildOptions } from "./types/config";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+  const {paths, mode, isDev, port} = options;
+
+  console.log('mode', mode, 'isDev', isDev)
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map': undefined,
+    devServer: isDev ?  buildDevServer(options) : undefined,
+  }
+}
