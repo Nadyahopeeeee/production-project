aedb8ffd86887a8bc8a9349b880494fdcb0a0fc0
Nadezhda Semenenko <semenenko.n@routeamgroup.com>
2023-10-20 09:34:47 +0300

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,9 +7,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader =  {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
-    // test: /\.svg$/i,
-    // issuer: /\.[jt]sx?$/,
-    // use: ['@svgr/webpack'],
+  }
+
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
   }
 
   const cssLoader = {
@@ -48,6 +65,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   return [
     fileLoader,
     svgLoader,
+    babelLoader,
     typescriptLoader,
     cssLoader,
   ]
